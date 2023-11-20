de2f515e104c8b3bea4f7c556b90bdcb4155b163
Lannee <3lirris@gmail.com>
2023-11-20 13:34:18 +0300

    MacAddr refactor

--- src/core/eth/mac.rs
+++ src/core/eth/mac.rs
@@ -1,13 +1,16 @@
 use std::error::Error;
 use regex::Regex;
 
-pub struct MacAddress<'a> {
-    address: &'a [u8; 6],
-}
+pub struct MacAddr(pub u8, pub u8, pub u8, pub u8, pub u8, pub u8);
 
-impl<'a> MacAddress<'a> {
-    fn from(bytes: &[u8; 6]) -> Result<MacAddress, Box<dyn Error>> {
-        Ok(MacAddress {address: bytes})
+impl<'a> MacAddr {
+
+    fn new(b0: u8, b1: u8, b2: u8, b3: u8, b4: u8, b5: u8) -> MacAddr {
+        MacAddr(b0, b1, b2, b3,b4,b5)
+    }
+
+    fn from(bytes: [u8; 6]) -> MacAddr {
+        MacAddr(bytes[0], bytes[1], bytes[2], bytes[3], bytes[4], bytes[5])
     }
 
     // fn from_str(string: &str) -> Result<MacAddress, Box<dyn Error>> {
@@ -16,7 +19,7 @@ impl<'a> MacAddress<'a> {
     //     }
     // }
 
-    fn bytes(self: &'a Self) -> &'a [u8; 6] {
-        self.address
+    fn bytes(self) -> [u8; 6] {
+        [self.0, self.1, self.2, self.3, self.4, self.5]
     }
 }
